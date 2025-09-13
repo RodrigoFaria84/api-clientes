1007f42d9150395871a88923e0a1ffb263f36df9
RodrigoFaria84 <rodrigofsilva84@outlook.com>
2025-09-12 22:03:46 -0300

    Service, Controler, mapeamento de entidades para postgre, configuração docker

---
+++ src/main/java/br/com/cotiinformatica/infrastructure/repositories/ClienteRepository.java
@@ -0,0 +1,25 @@
+package br.com.cotiinformatica.infrastructure.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cotiinformatica.domain.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+
+	boolean existsByCpf(String cpf);
+	
+	boolean existsByEmail(String email);
+	
+	Optional<Cliente> findByCpf(String cpf);
+	
+	Optional<Cliente> findByEmail(String email);
+	
+	Page<Cliente> findByAtivoTrue(Pageable pageable);
+}
